e1907e0e57c4c04f47d367e9fee29b46e5c8040c
mkorrea <mathscorrea@gmail.com>
2024-04-17 17:11:21 -0300

    create trailer button for info page

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,67 +1,96 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import api from '../../services/api'
-import './movie_info.css'
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import "./movie_info.css";
 
-function Movie () {
-    const { id } = useParams();
-    const navigate = useNavigate()
-    const [ movie, setMovie] = useState({});
-    const [ loading, setLoading ] = useState(true);
+function Movie() {
+   const { id } = useParams();
+   const navigate = useNavigate();
+   const [movie, setMovie] = useState({});
+   const [loading, setLoading] = useState(true);
 
-    useEffect( () => {
-        async function loadMovie() {
-            await api.get(`/movie/${id}`, {
-                params:{
-                    api_key: '9506b5a090386e5ff12c1d1300fb977f',
-                    language: 'pt-BR',
-                }
+   useEffect(() => {
+      async function loadMovie() {
+         await api
+            .get(`/movie/${id}`, {
+               params: {
+                  api_key: "9506b5a090386e5ff12c1d1300fb977f",
+                  language: "pt-BR",
+               },
             })
-            .then( (response) => {
-                setMovie(response.data)
-                setLoading(false)
-                //definindo que 'movie' vai ter a api com o id do filme selecionado
+            .then((response) => {
+               setMovie(response.data);
+               setLoading(false);
+               //definindo que 'movie' vai ter a api com o id do filme selecionado
             })
             // executa .then caso ele consiga carregar a api
-            .catch( () => {
-                navigate("/", { replace: true }) 
-                //redireciona para para a pagina inicial
-                // replace faz com que o usuário não consiga voltar para a página anterior (com as setinhas do navegador, por exemplo)
-            })
-            // executa .catch caso não carregue a api
-        }
-        loadMovie()
+            .catch(() => {
+               navigate("/", { replace: true });
+               //redireciona para para a pagina inicial
+               // replace faz com que o usuário não consiga voltar para a página anterior (com as setinhas do navegador, por exemplo)
+            });
+         // executa .catch caso não carregue a api
+      }
+      loadMovie();
 
-        return () => {
-            console.log('desmontou')
-        }
-    }, [id, navigate])
-    
-    if(loading) {
-        return(
-            <div className='loading'>
-                <h1> Carregando detalhes... </h1>
-            </div>
-        )
-    }
+      return () => {
+         console.log("desmontou");
+      };
+   }, [id, navigate]);
 
-    return (
-        <div className='movie-info'>
-            <h1> {movie.title} </h1>
-            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+   function saveMovie(){
+      const myList = localStorage.getItem('@primeflix')
 
-            <h3> Sinopse </h3>
-            <span> {movie.overview} </span>
-            <strong> Avaliação: {movie.vote_average}/10 </strong>
+      let savedMovies = JSON.parse(myList) || []
 
-            <section className='area-buttons'>
-                <button>Salvar</button>
-                <button>
-                    <a href={`https://youtube.com/results?search_query=${movie.title} trailer`} target='blank' rel='external'>Trailer</a>
-                </button>
-            </section>
-        </div>
-    )
+      const hasMovie = savedMovies.some( (movieSaved) => movieSaved.id === movie.id )
+      if(hasMovie){
+         alert('Esse filme já está na lista')
+         return;
+      }
+      
+      savedMovies.push(movie)
+      localStorage.setItem('@primeflix', JSON.stringify(savedMovies))
+      alert('filme salvo')
+   }
+
+
+
+   if (loading) {
+      return (
+         <div className="loading">
+            <h1> Carregando detalhes... </h1>
+         </div>
+      );
+   }
+
+
+   return (
+      <div className="movie-info">
+         <h1> {movie.title} </h1>
+         <img
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt={movie.title}
+         />
+
+         <h3> Sinopse </h3>
+         <span> {movie.overview} </span>
+         <strong> Avaliação: {movie.vote_average}/10 </strong>
+
+         <section className="area-buttons">
+            <button onClick={saveMovie}>Salvar</button>
+            <button>
+               <a
+                  href={`https://youtube.com/results?search_query=${movie.title} trailer`}
+                  target="blank"
+                  rel="external"
+               >
+                  Trailer
+               </a>
+            </button>
+         </section>
+      </div>
+   );
 }
 
 export default Movie;
